1189df86b3e41f912f237bb46d13d0a777a11cc7
likewater <lakebayomackway@gmail.com>
2017-06-10 04:17:53 -0700

    add png's, add fragment, add activity

---
+++ app/src/main/java/com/likewater/articleone/ui/RepDetailActivity.java
@@ -0,0 +1,15 @@
+package com.likewater.articleone.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.likewater.articleone.R;
+
+public class RepDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rep_detail);
+    }
+}
