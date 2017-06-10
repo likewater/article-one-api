c27014259accd70975bb61b57d77c7578a87e8d9
likewater <lakebayomackway@gmail.com>
2017-06-10 02:31:48 -0700

    add intent to about page

--- app/src/main/java/com/likewater/articleone/ui/AboutActivity.java
+++ app/src/main/java/com/likewater/articleone/ui/AboutActivity.java
@@ -1,43 +1,27 @@
 package com.likewater.articleone.ui;
+
 import android.net.Uri;
 import android.os.Bundle;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+import com.likewater.articleone.R;
+import butterknife.Bind;
 
-        import android.content.Intent;
-        import android.support.v7.app.AppCompatActivity;
-
-
-        import com.likewater.articleone.R;
-        import butterknife.Bind;
-        import butterknife.ButterKnife;
-
-public class AboutActivity extends AppCompatActivity {
-    //@Bind(R.id.websiteTextView) TextView mWebsiteLabel;
-    public String phoneNumber = "2125551212";
+public class AboutActivity extends AppCompatActivity implements View.OnClickListener{
+    private static final String TAG = "url";
+    @Bind(R.id.websiteTextView) TextView mWebsiteLabel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
-        //mWebsiteLabel.setOnClickListener(this);
-        dialPhoneNumber(phoneNumber);
-
-    }
-    //Intent intent = getIntent();
-
-//    Intent webIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.propublica.org/datastore/api/propublica-congress-api"));
-//    startActivity(webIntent);
-
-
-
-
-
-
-    public void dialPhoneNumber(String phoneNumber) {
-        Intent intent = new Intent(Intent.ACTION_DIAL);
-        intent.setData(Uri.parse("tel:" + phoneNumber));
-        if (intent.resolveActivity(getPackageManager()) != null) {
-            startActivity(intent);
-        }
     }
 
+   @Override
+    public void onClick(View v) {
+            Intent webIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.propublica.org/datastore/api/propublica-congress-api"));
+            startActivity(webIntent);
+    }
 }
