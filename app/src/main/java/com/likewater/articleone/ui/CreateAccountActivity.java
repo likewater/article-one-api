d1acc53611ea4038bde1552b369356cf8daaff0a
Blake <lakebayomackway@gmail.com>
2017-06-12 08:55:27 -0700

    add create account activity

---
+++ app/src/main/java/com/likewater/articleone/ui/CreateAccountActivity.java
@@ -0,0 +1,15 @@
+package com.likewater.articleone.ui;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.likewater.articleone.R;
+
+public class CreateAccountActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+    }
+}
